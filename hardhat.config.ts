6cec5cf81fb5c8c89f16c4e1e538c6625d64439e
Antho <anthony.rolland@proton.me>
2024-03-25 23:38:50 +0100

    :wrench: Configuration pour le déploiement sur Sépolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,27 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: [process.env.PRIVATE_KEY || ""],
+      chainId: 5
+    },
+    localhost: {
+      url: "http://localhost:8545",
+      chainId: 31337
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || ""
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS ? true : false
+  }
 };
 
 export default config;
