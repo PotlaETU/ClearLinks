a29629dcbd9d37348c45ca3edf234ee6169781cb
Antho <anthony.rolland@proton.me>
2024-03-26 21:34:38 +0100

    :zap: Optimisations de variables

--- scripts/donation.ts
+++ scripts/donation.ts
@@ -1,10 +1,5 @@
 import { ethers, network } from "hardhat";
 import { verify } from "../utils/verification";
-import "dotenv/config";
-
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "";
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 async function main() {
   const DonationContract = await ethers.getContractFactory("Donation");
