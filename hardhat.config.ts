a815d4c1000858a91881272d86982707400d0400
Antho <anthony.rolland@proton.me>
2024-03-26 21:34:52 +0100

    :zap: Optimisations du fichier de config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,12 +4,10 @@ import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
-  defaultNetwork: "hardhat",
   networks: {
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL || "",
       accounts: [process.env.PRIVATE_KEY || ""],
-      chainId: 5
     },
     localhost: {
       url: "http://localhost:8545",
@@ -20,7 +18,7 @@ const config: HardhatUserConfig = {
     apiKey: process.env.ETHERSCAN_API_KEY || ""
   },
   gasReporter: {
-    enabled: process.env.REPORT_GAS ? true : false
+    enabled: true
   }
 };
 
